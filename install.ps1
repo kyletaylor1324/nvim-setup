f81e04f4c2944b4593ebbb7916209b97841c8cce
Kyle Taylor <kyle1324@hotmail.com>
2025-01-31 16:11:13 -0600

    add ollama stuff

--- install.ps1
+++ install.ps1
@@ -8,6 +8,10 @@ scoop install python
 scoop install nvm
 nvm install lts 
 scoop install gcc
+scoop install ollama
+
+ollama pull deepseek-r1:14b
+ollama pull qwen2.5-coder:7b
 
 
 Copy-Item -Path "./config/nvim/" -Destination "$env:LOCALAPPDATA" -Recurse
