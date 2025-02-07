a1595b88074aad6106cf42ce9ebaee27d43ba24d
Kyle Taylor <kyle.taylor@americanfidelity.com>
2025-02-07 11:10:41 -0600

    add flash/telescope searching

---
+++ lua/plugins/flash-nvim.lua
@@ -0,0 +1,91 @@
+return {
+  "folke/flash.nvim",
+  event = "VeryLazy",
+  dependencies = {
+    {
+      "AstroNvim/astrocore",
+      opts = {
+        mappings = {
+          x = {
+            ["s"] = {
+              function() require("flash").jump() end,
+              desc = "Flash",
+            },
+            ["R"] = {
+              function() require("flash").treesitter_search() end,
+              desc = "Treesitter Search",
+            },
+            ["S"] = {
+              function() require("flash").treesitter() end,
+              desc = "Flash Treesitter",
+            },
+          },
+          o = {
+            ["r"] = {
+              function() require("flash").remote() end,
+              desc = "Remote Flash",
+            },
+            ["R"] = {
+              function() require("flash").treesitter_search() end,
+              desc = "Treesitter Search",
+            },
+            ["s"] = {
+              function() require("flash").jump() end,
+              desc = "Flash",
+            },
+            ["S"] = {
+              function() require("flash").treesitter() end,
+              desc = "Flash Treesitter",
+            },
+          },
+          n = {
+            ["s"] = {
+              function() require("flash").jump() end,
+              desc = "Flash",
+            },
+            ["S"] = {
+              function() require("flash").treesitter() end,
+              desc = "Flash Treesitter",
+            },
+          },
+        },
+      },
+    },
+    {
+      "nvim-telescope/telescope.nvim",
+      optional = true,
+      opts = function(_, opts)
+        local function flash(prompt_bufnr)
+          require("flash").jump {
+            pattern = "^",
+            label = { after = { 0, 0 } },
+            search = {
+              mode = "search",
+              exclude = {
+                function(win) return vim.bo[vim.api.nvim_win_get_buf(win)].filetype ~= "TelescopeResults" end,
+              },
+            },
+            action = function(match)
+              local picker = require("telescope.actions.state").get_current_picker(prompt_bufnr)
+              picker:set_selection(match.pos[1] - 1)
+              vim.cmd "stopinsert" -- Exit Insert mode and return to Normal mode
+            end,
+          }
+        end
+        opts.defaults = vim.tbl_deep_extend("force", opts.defaults or {}, {
+          mappings = {
+            n = { s = flash },
+            i = { ["<c-s>"] = flash },
+          },
+        })
+      end,
+    },
+  },
+  opts = {
+    modes = {
+      search = {
+        enabled = false,
+      },
+    },
+  },
+}
