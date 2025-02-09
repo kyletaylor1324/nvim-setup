26c6c878e344153c784650c0af19981cba8444b0
Kyle Taylor <kyle1324@hotmail.com>
2025-02-08 18:44:20 -0600

    auto restore sessions

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -22,5 +22,30 @@ return {
         ["<C-t>"] = { "<Esc><Cmd>ToggleTerm<CR>", desc = "Toggle terminal" },
       },
     },
+    sessions = {
+      -- Configure auto saving
+      autosave = {
+        last = true, -- auto save last session
+        cwd = true, -- auto save session for each working directory
+      },
+    },
+    autocmds = {
+      -- disable alpha autostart
+      alpha_autostart = false,
+      restore_session = {
+        {
+          event = "VimEnter",
+          desc = "Restore previous directory session if neovim opened with no arguments",
+          nested = true, -- trigger other autocommands as buffers open
+          callback = function()
+            -- Only load the session if nvim was started with no args
+            if vim.fn.argc(-1) == 0 then
+              -- try to load a directory session using the current working directory
+              require("resession").load(vim.fn.getcwd(), { dir = "dirsession" })
+            end
+          end,
+        },
+      },
+    },
   },
 }
